3583aaf7e9799bdbb04d151bb7201bc7468ca612
JosephatNgugi <Josephat.muriu@student.moringaschool.com>
2022-04-11 14:15:24 +0300

    Fix the highlight directive to highlight the highest upvoted quote.

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -13,12 +13,18 @@ export class QuotesComponent implements OnInit {
     new Quote(
       1,
       'Courage is not the absence of fear. It is the presence of fear, yet the will to move on',
-      1,
+      0,
       0,
       'Nelson Mandela'
     ),
+    new Quote(2, 'QUOTE HERE', 0, 0, 'Author Name'),
+    new Quote(3, 'QUOTE HERE', 0, 0, 'Author Name'),
+    new Quote(4, 'QUOTE HERE', 0, 0, 'Author Name'),
+    new Quote(5, 'QUOTE HERE', 0, 0, 'Author Name'),
   ];
 
+  highestVote: number[] = this.userquotes.map(quoter=>quoter.upvote)
+  highest = Math.max(...this.highestVote)
   constructor() {}
 
   ngOnInit(): void {}
